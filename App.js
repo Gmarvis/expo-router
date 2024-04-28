c4ee1f4227c6dfe5080bdf34de357ed3b9d5fea2
gmarvis <sgmarvis@gmail.com>
2024-04-28 16:30:26 +0100

    basic config

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import { View, Text } from "react-native";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text>Expo Router App</Text>
+        </View>
+    );
 }
